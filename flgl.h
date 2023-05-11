3fb4b1f049da024781d06b3e03b930ae8b0031aa
collebrusco <collebrusco.frank@gmail.com>
2023-05-10 19:02:28 -0500

    reorganize flgl

---
+++ flgl.h
@@ -0,0 +1,13 @@
+//
+//  flgl.h
+//  graphics-library-interface
+//
+//  Created by Frank Collebrusco on 5/10/23.
+//
+
+#ifndef flgl_h
+#define flgl_h
+
+#include "flgl/Graphics.h"
+
+#endif /* flgl_h */
