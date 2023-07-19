21b3df1fbc8a36be14777c33d3a4ac1a74f78b66
collebrusco <collebrusco.frank@gmail.com>
2023-07-19 07:53:38 -0500

    move user includable files to ./inc

---
+++ inc/flgl_math.h
@@ -0,0 +1,26 @@
+//
+//  flgl_math.h
+//  flgl-tester
+//
+//  Created by Frank Collebrusco on 5/30/23.
+//
+/*
+ FLGL_Math provides commonly re-used matrix calculations.
+ */
+
+#ifndef flgl_math_h
+#define flgl_math_h
+
+#include <glm/ext/matrix_transform.hpp>
+
+// Model matrix calculators
+// These generate model matricies from 2D or 3D position, rotation, scale, and optionally anchor points.
+glm::mat4 genModelMat2d(glm::vec2 pos, float rotation, glm::vec2 scale, glm::vec2 anchor);
+glm::mat4 genModelMat2d(glm::vec2 pos, float rotation, glm::vec2 scale);
+glm::mat4 genModelMat3d(glm::vec3 pos, glm::vec3 rotation, glm::vec3 scale, glm::vec3 anchor);
+glm::mat4 genModelMat3d(glm::vec3 pos, glm::vec3 rotation, glm::vec3 scale);
+
+// vector help
+glm::vec2 angleToVector(float degrees);
+
+#endif /* flgl_math_h */
