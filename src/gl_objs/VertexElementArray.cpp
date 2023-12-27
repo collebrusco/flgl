20851f14d09350638e56f0a16a27d892acb0cd84
collebrusco <collebrusco.frank@gmail.com>
2023-12-27 14:39:01 -0600

    add new element array abs

---
+++ src/gl_objs/VertexElementArray.cpp
@@ -0,0 +1,2 @@
+#include "VertexElementArray.h"
+
---
+++ src/gl_objs/VertexElementArray.h
@@ -0,0 +1,11 @@
+#ifndef VERTEX_ELEMENT_ARRAY_H
+#define VERTEX_ELEMENT_ARRAY_H
+
+class VertexElementArray : public VertexArray {
+
+public:
+	VertexElementArray() = default;
+};
+
+
+#endif
