839bfd566b37e5f3c235140abca40442a996ba18
collebrusco <collebrusco.frank@gmail.com>
2023-12-27 20:34:23 -0600

    fixedgit add inc/glm.h

---
+++ inc/glm.h
@@ -0,0 +1,5 @@
+#ifndef FLGL_GLM_H
+#define FLGL_GLM_H
+#include <glm/glm.hpp>
+#endif
+
--- src/Verticies.h
+++ src/Verticies.h
@@ -2,12 +2,12 @@
 #define VERTICIES_H
 
 #include "gfx.h"
-#include "VertexBuffer.h"
+#include "gl_objs/VertexBuffer.h"
 
 struct __attribute__((packed)) Vt3p2u {
-	vec3 pos;
-	vec2 uv;
+	glm::vec3 pos;
+	glm::vec2 uv;
 }; typedef Vt3p2u Vt_classic;
-x
+
 
 #endif
