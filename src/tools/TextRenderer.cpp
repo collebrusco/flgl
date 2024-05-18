b0c79daa988b5acb1fa603a84ddcd7e00b17ae87
collebrusco <collebrusco.frank@gmail.com>
2024-05-18 02:29:50 -0500

    add testmain

--- src/tools/TextRenderer.cpp
+++ src/tools/TextRenderer.cpp
@@ -101,3 +101,80 @@ void TextRenderer::destroy() {
 	string_mesh.destroy();
 }
 
+// ******* TESTMAIN ********
+// setup as:
+// src/
+// |__main.cpp
+// lib/
+// |__flgl/
+// |__sw/
+
+// #include <flgl.h>
+// #include <flgl/tools.h>
+// #include <flgl/glm.h>
+// #include <iostream>
+// using namespace std;
+// using namespace glm;
+// #include <flgl/logger.h>
+// #include "../lib/sw/Stopwatch.h"
+// LOG_MODULE(main);
+
+// #define min(a,b) ((a)<(b)?(a):(b))
+// #define max(a,b) ((a)>(b)?(a):(b))
+// #define clamp(a,l,h) (min(h,max(l,a)))
+
+// static Stopwatch timer(MICROSECONDS);
+
+// int main() {
+
+// 	gl.init();
+// 	window.create("text", 1280, 720);
+
+// 	TextRenderer tr;
+// 	TextRenderer::init_text_rendering();
+// 	tr.init();
+
+// 	timer.start();
+// 	tr.set_text("hello text!\nwow.. a new line!");
+// 	float en = timer.stop();
+
+// 	LOG_DBG("set time %.2fus", en);
+
+// 	int i = 500;
+// 	int size = 10;
+// 	int c = 0;
+
+// 	vec3 clr{0.,0.,0.};
+// 	float theta = 0;
+
+// 	while (!window.should_close()) {
+// 		if (window.keyboard[GLFW_KEY_ESCAPE].down) window.close();
+// 		gl.clear();
+
+// 		size += window.mouse.scroll.y / 10;
+// 		size = clamp(size, 1, 30);
+
+// 		theta += 0.01;
+// 		clr.r = (sin(theta) / 2) + 1;
+// 		clr.g = (sin(theta+13124214) / 2) + 1;
+// 		clr.b = (sin(theta-999393) / 2) + 1;
+// 		tr.set_color(clr);
+
+// 		timer.reset_start();
+// 		tr.render(window.mouse.pos.x, window.mouse.pos.y, size);
+// 		float ren = timer.stop();
+// 		// LOG_DBG("render %.2fus", en);
+// 		if (c==0x40) { c = 0;
+// 			timer.reset_start();
+// 			tr.set_text("last set %.0fus\nlast ren %.0fus", en, ren);
+// 			en = timer.stop();
+// 		}
+// 		window.update(); c++;
+// 	}
+
+// 	gl.destroy();
+
+// 	return 0;
+// }
+
+
