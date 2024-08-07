dc7e7b6460ec0a6700c88611e7a3b3373edf6527
collebrusco <collebrusco.frank@gmail.com>
2024-08-07 13:13:14 -0500

    improve setup script

--- user/setup.sh
+++ user/setup.sh
@@ -2,8 +2,9 @@
 
 if [ -f ./lib/flgl/user/user_CMakeLists.txt ]; then
 	cp ./lib/flgl/user/user_CMakeLists.txt ./CMakeLists.txt
-	cp ./lib/flgl/.gitignore/ ./
-	mkdir src
+	cp ./lib/flgl/user/user_Makefile ./Makefile
+	cp ./lib/flgl/.gitignore ./
+	mkdir -p src
 	cp ./lib/flgl/user/hellowindow.cpp ./src/main.cpp
 else
 	echo "Run this from your project directory with flgl at ./lib/flgl to bring in a CMakeLists, gitignore, & place an flgl hello world main at src/main.cpp"
