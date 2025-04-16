93e5be7fc486419b7ca978259e0fdac851144cff
collebrusco <collebrusco.frank@gmail.com>
2025-04-16 01:23:38 -0500

    specific includes

---
+++ inc/flgl/driver.h
@@ -0,0 +1,4 @@
+#ifndef FLGL_INC_DRIVER
+#define FLGL_INC_DRIVER
+#include "../../src/tools/Driver.h"
+#endif
---
+++ inc/flgl/geometry.h
@@ -0,0 +1,4 @@
+#ifndef FLGL_INC_GEOMETRY
+#define FLGL_INC_GEOMETRY
+#include "../../src/tools/geometry.h"
+#endif
